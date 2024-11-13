56e0f77d6069a26ea523602ee8fef18a8057174a
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2024-11-12 19:22:35 -1000

    feat: imlement initial waitlist feature

--- backend/src/trpc/router.ts
+++ backend/src/trpc/router.ts
@@ -1,18 +1,31 @@
-import { Waitlist } from "../schema";
-import { initalizeSeating } from "../core/db";
+import { WaitlistSchema } from "../schema";
+import {
+  initalizeSeating,
+  listCollections,
+  waitlist,
+  databases,
+} from "../core/db";
 import { publicProcedure, router } from "./trpc";
-
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 export const appRouter = router({
   seats: publicProcedure.query(() => {
     return initalizeSeating();
   }),
   waitlist: {
     get: publicProcedure.query(() => "hi from tRPC waitlist"),
-    add: publicProcedure.input(Waitlist).mutation((options) => {
-      console.log(options.input);
-      return "Added to waitlist.";
+    add: publicProcedure.input(WaitlistSchema).mutation((options) => {
+      const added = waitlist.add(options.input);
+      console.log(added);
+      return added;
     }),
+  },
+  collections: {
+    get: publicProcedure.query(() => listCollections()),
+  },
+  databases: {
+    getAll: publicProcedure.query(() => databases.list()),
   },
 });
 
 export type AppRouter = typeof appRouter;
+export type RouterInputs = inferRouterInputs<AppRouter>;
