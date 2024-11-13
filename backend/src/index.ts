2b0df0b47271c6a143df11a5346fec7abe95088b
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2024-11-12 16:10:19 -1000

    chores: remove unused routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,11 +12,6 @@ const app = express();
 
 app.use(cors(), bodyParser.json());
 
-app.get("/", async (req, res) => {
-  const seats = initalizeSeating();
-  res.json(seats);
-});
-
 app.use(
   process.env.TRPC_ENDPOINT,
   trpcExpress.createExpressMiddleware({
@@ -24,11 +19,6 @@ app.use(
     createContext,
   })
 );
-
-app.post("/add-to-waitlist", async (req, res) => {
-  console.log(req.body);
-  res.send("Add to waitlist body");
-});
 
 app.listen(process.env.PORT, () => {
   console.log("Listening on port ", process.env.PORT);
