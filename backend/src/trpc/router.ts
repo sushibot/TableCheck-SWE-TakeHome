a4f8d6a56d74e3a15d61ba91522cc215ac224c6f
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2024-11-14 11:40:45 -1000

    refactor: update waitlist add mutation

--- backend/src/trpc/router.ts
+++ backend/src/trpc/router.ts
@@ -1,4 +1,5 @@
-import { WaitlistSchema } from "../schema";
+import { WaitlistInput, WaitlistOutput } from "../schema";
+import { waitlistQueue } from "../core/queue";
 import {
   initalizeSeating,
   listCollections,
@@ -6,17 +7,49 @@ import {
   databases,
 } from "../core/db";
 import { publicProcedure, router } from "./trpc";
-import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+import type { inferRouterInputs } from "@trpc/server";
+
 export const appRouter = router({
   seats: publicProcedure.query(() => {
     return initalizeSeating();
   }),
   waitlist: {
     get: publicProcedure.query(() => "hi from tRPC waitlist"),
-    add: publicProcedure.input(WaitlistSchema).mutation((options) => {
-      const added = waitlist.add(options.input);
-      console.log(added);
-      return added;
+    add: publicProcedure
+      .output(WaitlistOutput)
+      .input(WaitlistInput)
+      .mutation(async (options) => {
+        const party = options.input;
+        try {
+          const added = await waitlist.add(party);
+          console.log({ data: added, line: "router.ts 22" });
+          if (added) {
+            waitlistQueue.enqueue({
+              partyName: party.partyName,
+              id: added?.insertedId.toString(),
+              size: party.size,
+            });
+          }
+          console.log({ queue: waitlistQueue.getStatus() });
+
+          return {
+            success: true,
+            message: `${party.partyName} successfully added to queue`,
+          };
+        } catch (error) {
+          console.log(`Error adding: ${party.partyName} to waitlist.`);
+          throw new Error();
+        }
+
+        // 3.
+        // add party to subscription list
+        // broadcast update to other SSE clients
+        // return timestamp, queue priority, estimated wait time
+      }),
+
+    queue: publicProcedure.input(WaitlistInput).query((options) => {
+      const name = options.input.partyName;
+      return `${name} has joined the waitlist queue.`;
     }),
   },
   collections: {
