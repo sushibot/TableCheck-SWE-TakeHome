dc44a548f1c5ad32350248204cfe448751a0e8d5
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2024-11-14 11:37:46 -1000

    feat: implement initial sse feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import { initalizeSeating } from "./core/db";
 
 import cors from "cors";
 import bodyParser from "body-parser";
@@ -8,6 +7,7 @@ import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./trpc/router";
 import { createContext } from "./trpc/trpc";
 
+import { waitlistQueue } from "./core/queue";
 const app = express();
 
 app.use(cors(), bodyParser.json());
@@ -19,6 +19,37 @@ app.use(
     createContext,
   })
 );
+
+app.get(`${process.env.ENDPOINT}/waitlist-queue`, (req, res) => {
+  res.writeHead(200, {
+    "access-control-allow-origin": "*",
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+    "X-Accel-Buffering": "no",
+    "Transfer-Encoding": "chunked",
+  });
+  console.log("Connected to queue.....");
+
+  const status = waitlistQueue.getStatus();
+
+  res.write(
+    `data: ${JSON.stringify({
+      type: "Initial State",
+      data: status,
+    })}\n\n`
+  );
+
+  const unsubscribe = waitlistQueue.subscribe((event) => {
+    if (!res.closed) {
+      res.write(`data: ${JSON.stringify(event)}\n\n`);
+    }
+  });
+
+  req.on("close", () => {
+    unsubscribe();
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Listening on port ", process.env.PORT);
